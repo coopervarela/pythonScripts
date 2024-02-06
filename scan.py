d466ade643538b01e6b2d28461c23d0eb6f165d6
Cooper <cooper.varela@gmail.com>
2024-02-06 11:12:25 -0500

    changed vars to better name

--- scan.py
+++ scan.py
@@ -8,16 +8,16 @@ userPort = input("Enter ports to scan:")
 
 port2Scan = userPort.split()
 
-for i in range(len(port2Scan)):
-    port2Scan[i] = int(port2Scan[i])
+for port in range(len(port2Scan)):
+    port2Scan[port] = int(port2Scan[port])
 
-for i in port2Scan:
+for port in port2Scan:
     try:
         socket.setdefaulttimeout(2)
         s = socket.socket()
-        s.connect((ip, i))
-        print("Connected on port: " + str(i))
+        s.connect((ip, port))
+        print("Connected on port: " + str(port))
     except:  
-        print("Couldn't connect on port: " + str(i))
+        print("Couldn't connect on port: " + str(port))
 
 
